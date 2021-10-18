207fd4a84908157881982d6aedf24aab068efb43
AndreiDE05 <ekeshev@gmail.com>
2021-10-18 10:35:07 +0000

    test 2

--- block.js
+++ block.js
@@ -6,12 +6,28 @@ class Block{
         this.lastHash = lastHash; // Hash des vorhergehenden Blocks
         this.hash = hash; // Eigener Hash
         this.data = data; // Daten
-
     }
 
+    toString(){
+        return `Block - 
+            Timestamp: ${this.timestamp}
+            Last Hash: ${this.lastHash}
+            Hash:      ${this.hash}
+            Data:      ${this.data}   `
+    }
 
+    static genesis(){  // 1. Block der Chain
+        return new this ("Genesis time", "------", "xABc1234", ["data1","data2"]);
+    }
 
-
-
+    static mineBlock(lastBlock, ownData){
+        const timestamp = Date.now(); // Zeit im ms seit 01.01.1970 | UNIX
+        const lastHash = lastBlock.hash;
+        const hash     = "Hash to Do";
+        
+        return new Block(timestamp, lastHash, hash, ownData);
+    }
 
 }
+
+module.exports = Block; //Export al Modul
--- dev-test.js
+++ dev-test.js
@@ -1,3 +1,14 @@
 // call: npm run dev-test
 // exit: Strg + C
-console.log("Halo");
+// restart: rs
+
+//Import
+const Block = require("./block")
+
+//const block = new Block("time","hash1","hash2","irgen dwas");
+//console.log(block.toString());
+
+//console.log(Block.genesis().toString());
+
+const testBlock = Block.mineBlock(Block.genesis(), "testData");
+console.log(testBlock.toString());
